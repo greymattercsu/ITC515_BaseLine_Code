39dbd1689aa21ce7bd4a1fe90e660710d35530ac
greymattercsu <41317773+greymattercsu@users.noreply.github.com>
2018-08-01 17:34:31 +1000

    changed formating and added comment
    Added missing comment and changed the formating of the code. Added spaces  to meeting the conventions of Java

--- src/BorrowBookControl.java
+++ src/BorrowBookControl.java
@@ -17,17 +17,32 @@ public class BorrowBookControl {
 	
 	private library library; // renamed variable L to library
 	private member member; // renamed variable M to member
-	private enum CONTROL_STATE { INITIALISED, READY, RESTRICTED, SCANNING, IDENTIFIED, FINALISING, COMPLETED, CANCELLED };
+
+	//changed formating for easier code reading
+	private enum CONTROL_STATE { INITIALISED, 
+		                         READY, 
+		                         RESTRICTED, 
+		                         SCANNING, 
+		                         IDENTIFIED, 
+		                         FINALISING, 
+		                         COMPLETED, 
+		                         CANCELLED };
+
+
 	private CONTROL_STATE state; 
 	
 	private List<book> PENDING;
 	private List<loan> COMPLETED;
+
 	private book book; //renamed variable B to book
 	
 	//renamed all the instances of L to Library
 	public BorrowBookControl() {
+
+
 		this.library = library.INSTANCE();//renamed variable L to Library
 		state = CONTROL_STATE.INITIALISED;
+
 	}
 	
 
@@ -36,6 +51,8 @@ public class BorrowBookControl {
 			throw new RuntimeException("BorrowBookControl: cannot call setUI except in INITIALISED state");
 			
 		this.ui = ui;
+
+
 		ui.setState(BorrowBookUI.UI_STATE.READY);
 		state = CONTROL_STATE.READY;		
 	}
@@ -66,6 +83,8 @@ public class BorrowBookControl {
 	public void Scanned(int bookId) {
 		
 		B = null;
+
+
 		if (!state.equals(CONTROL_STATE.SCANNING)) {
 			throw new RuntimeException("BorrowBookControl: cannot call bookScanned except in SCANNING state");
 		}
@@ -73,7 +92,7 @@ public class BorrowBookControl {
 		//renamed variable L to Library
 		B = library.Book(bookId);
 
-		
+
 		if (B == null) {
 			ui.display("Invalid bookId");
 			return;
@@ -102,6 +121,8 @@ public class BorrowBookControl {
 	
 	
 	public void Complete() {
+
+
 		if (PENDING.size() == 0) {
 			cancel();
 		}
@@ -110,6 +131,8 @@ public class BorrowBookControl {
 			for (book b : PENDING) {
 				ui.display(b.toString());
 			}
+
+
 			COMPLETED = new ArrayList<loan>();
 			ui.setState(BorrowBookUI.UI_STATE.FINALISING);
 			state = CONTROL_STATE.FINALISING;
@@ -118,23 +141,34 @@ public class BorrowBookControl {
 
 
 	public void commitLoans() {
+
+
 		if (!state.equals(CONTROL_STATE.FINALISING)) {
 			throw new RuntimeException("BorrowBookControl: cannot call commitLoans except in FINALISING state");
-		}	
+		}
+
+
 		for (book b : PENDING) {
-			loan loan = library.issueLoan(b, M);
+			loan loan = library.issueLoan(b, M);//renamed variable L to Library
 			COMPLETED.add(loan);			
 		}
+
+
 		ui.display("Completed Loan Slip");
+
+
 		for (loan loan : COMPLETED) {
 			ui.display(loan.toString());
 		}
+
+
 		ui.setState(BorrowBookUI.UI_STATE.COMPLETED);
 		state = CONTROL_STATE.COMPLETED;
 	}
 
 	
 	public void cancel() {
+
 		ui.setState(BorrowBookUI.UI_STATE.CANCELLED);
 		state = CONTROL_STATE.CANCELLED;
 	}
