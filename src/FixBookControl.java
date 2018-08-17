bc1a83ef1cd82d99b7e4a4e1bdc85b5d9b922cc9
greymattercsu <41317773+greymattercsu@users.noreply.github.com>
2018-08-17 21:36:25 +1000

    changed as per reviewer's suggestion
    the line spacing is fixed.

--- src/FixBookControl.java
+++ src/FixBookControl.java
@@ -9,37 +9,26 @@
 public class FixBookControl {
 	
 	private FixBookUI ui;
-
 	//changed CONTROL_STATE to ControlState
-	private enum ControlState { INITIALISED, 
-	                            READY, 
-	                            FIXING }; 
-
+	private enum ControlState { INITIALISED, READY, FIXING };
 	private ControlState state; //changed CONTROL_STATE to ControlState
-	
 	private Library library; //changed l in librart to L for Library since it is class
 	private Book currentBook; //changed b in book to B for Book since it is class
-
 
 	public FixBookControl() {
 		this.library = library.INSTANCE();
 		state = ControlState.INITIALISED; //changed CONTROL_STATE to ControlState
 	}
 	
-	
 	public void setUI(FixBookUI ui) {
-
 		//changed CONTROL_STATE to ControlState
 		if (!state.equals(ControlState.INITIALISED)) {
 			throw new RuntimeException("FixBookControl: cannot call setUI except in INITIALISED state");
 		}	
-
-		
 		this.ui = ui;
 		ui.setState(FixBookUI.UiState.READY); //changed UI_STATE to UiState
 		state = ControlState.READY;	 //changed CONTROL_STATE to ControlState	
 	}
-
 
 	public void bookScanned(int bookId) {
 		if (!state.equals(ControlState.READY)) {
@@ -51,8 +40,6 @@ public class FixBookControl {
 			ui.display("Invalid bookId");
 			return;
 		}
-
-
 		if (!currentBook.Damaged()) {
 			ui.display("\"Book has not been damaged");
 			return;
@@ -63,9 +50,7 @@ public class FixBookControl {
 		state = ControlState.FIXING;  //changed CONTROL_STATE to ControlState		
 	}
 
-
 	public void fixBook(boolean fix) {
-
 		//changed CONTROL_STATE to ControlState
 		if (!state.equals(ControlState.FIXING)) {
 			throw new RuntimeException("FixBookControl: cannot call fixBook except in FIXING state");
@@ -78,14 +63,11 @@ public class FixBookControl {
 		state = ControlState.READY; //changed CONTROL_STATE to ControlState		
 	}
 
-	
 	public void scanningComplete() {
-
 		//changed CONTROL_STATE to ControlState
 		if (!state.equals(ControlState.READY)) {
 			throw new RuntimeException("FixBookControl: cannot call scanningComplete except in READY state");
 		}	
-
 		ui.setState(FixBookUI.UiState.COMPLETED); //changed UI_STATE to UiState		
 	}
 
