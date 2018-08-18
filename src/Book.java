a5512ad683f308c9d3f1dd0862fddd2787dd8ae2
Bijan Dhakal <dhakal.bijan@gmail.com>
2018-08-18 23:24:20 +1000

    AFTER REVIEW: Removing unnecessary comments

--- src/Book.java
+++ src/Book.java
@@ -10,7 +10,7 @@
 import java.io.Serializable;
 
 
-@SuppressWarnings("serial") //it informs the compiler that it should not produce the serialversionuid warning.
+@SuppressWarnings("serial") 
 public class Book implements Serializable {
 	
 	private String T;
@@ -21,14 +21,6 @@ public class Book implements Serializable {
 	private State state;
 	
 	
-	/**
-	constructor initialization
-	
-	@param author
-	@param title
-	@param callNo
-	@param id
-	 */
 	public Book(String author, String title, String callNo, int id) {
 		this.A = author;
 		this.T = title;
@@ -37,10 +29,7 @@ public class Book implements Serializable {
 		this.state = State.AVAILABLE;
 	}
 	
-	/**
-	overrides toSting method
-	@return String description of Book
-	 */
+	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Book: ").append(ID).append("\n")
@@ -52,49 +41,33 @@ public class Book implements Serializable {
 		return sb.toString();
 	}
 
-	/**
-	Getter for ID
-	@return ID
-	 */
+	
 	public Integer ID() {
 		return ID;
 	}
 
-	/**
-	Getter for Title
-	@return title
-	 */
+	
 	public String title() {
 		return T;
 	}
 
 
-	/**
-	@return true if book is available
-	 */
+	
 	public boolean available() {
 		return state == State.AVAILABLE;
 	}
 
-	/**
-	@return true if book is on loan
-	 */
+	
 	public boolean onLoan() {
 		return state == State.ON_LOAN;
 	}
 
-	/**
-	@return true if book is damaged
-	 */
+
 	public boolean damaged() {
 		return state == State.DAMAGED;
 	}
 
-	/**
-	borrow method
-	change the state of book to on_loan if book was available else
-	Display the message why book can not be borrowed
-	 */
+	
 	public void borrow() {
 		if (state.equals(State.AVAILABLE)) {
 			state = State.ON_LOAN;
@@ -105,12 +78,7 @@ public class Book implements Serializable {
 		
 	}
 
-	/**
-	return book method
-	@param DAMAGED
-	if book was on loan chnage the book state to wither damaged or availiable
-	if book cannot be return display message why.
-	 */
+	
 	public void returnBook(boolean isDamaged) {
 		if (state.equals(State.ON_LOAN)) {
 			if (isDamaged) {
@@ -125,11 +93,7 @@ public class Book implements Serializable {
 		}		
 	}
 
-	/**
-	repair method
-	if book was damaged changed it's state to available
-	else display message why book cannot be repair
-	*/	
+		
 	public void repair() {
 		if (state.equals(State.DAMAGED)) {
 			state = State.AVAILABLE;
